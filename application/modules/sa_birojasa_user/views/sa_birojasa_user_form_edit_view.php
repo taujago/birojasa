de3cb48edc6da0611dcdaee695906afc9068ed2e
NizarHafizulllah <nizarhafizullah66@gmail.com>
2016-04-10 14:30:39 +0700

    admin theme

--- application/modules/sa_birojasa_user/views/sa_birojasa_user_form_edit_view.php
+++ application/modules/sa_birojasa_user/views/sa_birojasa_user_form_edit_view.php
@@ -5,66 +5,69 @@
         <!-- Main content -->
         <form id="form_edit" class="form-horizontal" method="post" action="<?php echo site_url("$this->controller/$action"); ?>" role="form"> 
 
-
-              <div class="panel panel-primary">
+ <div class="panel panel-primary">
       <div class="panel-heading">
-      <strong><h3 class="panel-title">Edit</h3></strong>
+      <strong><h3 class="panel-title">Tambah data </h3></strong>
     </div>
     <div class="panel-body" id="data_umum">
 
-    <div class="form-group">
-      <label class="col-sm-3 control-label">Nama Biro Jasa</label>
+	<div class="form-group">
+      <label class="col-sm-3 control-label">Email</label>
       <div class="col-sm-9">
-        <input type="hidden" name="id" id="id" value="<?php echo $id; ?>">
-        <input type="text" name="nama" id="nama" class="form-control input-style" placeholder="Nama" value="<?php echo $nama; ?>">
+        <input type="text" name="email" id="email" class="form-control input-style" placeholder="Email . . ." value="<?php echo isset($email)?$email:""; ?>">
       </div>
     </div>
+    
     <div class="form-group">
-      <label class="col-sm-3 control-label">No. SIUP</label>
+      <label class="col-sm-3 control-label">Password</label>
       <div class="col-sm-9">
-        <input type="text" name="no_siup" id="no_siup" class="form-control input-style" placeholder="Nomor Siup . . ." value="<?php echo $no_siup; ?>">
+        <input type="password" name="p1" id="p1" class="form-control input-style" placeholder="Password.."  >
       </div>
     </div>
-    <div class="form-group">
-      <label class="col-sm-3 control-label">NO. NPWP</label>
+      <div class="form-group">
+      <label class="col-sm-3 control-label">Ulangi Password</label>
       <div class="col-sm-9">
-        <input type="text" name="no_npwp" id="no_npwp" class="form-control input-style" placeholder="NO. NPWP . . ." value="<?php echo $no_npwp; ?>">
+        <input type="password" name="p2" id="p2" class="form-control input-style" placeholder="Ulangi Password.."  >
       </div>
     </div>
+
     <div class="form-group">
-      <label class="col-sm-3 control-label">No. TDP</label>
+      <label class="col-sm-3 control-label">Nama </label>
       <div class="col-sm-9">
-        <input type="text" name="no_tdp" id="no_tdp" class="form-control input-style" placeholder="No. TDP . . ." value="<?php echo $no_tdp; ?>">
+       
+        <input type="text" name="nama" id="nama" class="form-control input-style" placeholder="Nama" 
+        value="<?php echo isset($nama)?$nama:""; ?>">
       </div>
     </div>
+    
+    
     <div class="form-group">
-      <label class="col-sm-3 control-label">Alamat</label>
+      <label class="col-sm-3 control-label">Birojasa </label>
       <div class="col-sm-9">
-      <textarea name="alamat" id="alamat" class="form-control input-style" placeholder="Alamat . . ."><?php echo $alamat; ?></textarea>
+       
+        <?php echo form_dropdown("birojasa_id",$arr_birojasa,$birojasa_id,'id="birojasa_id" class="form-control input-style"'); ?>
       </div>
     </div>
-    <div class="form-group">
-      <label class="col-sm-3 control-label">No. Telp</label>
-      <div class="col-sm-9">
-        <input type="text" name="telp" id="telp" class="form-control input-style" placeholder="No. Telp" value="<?php echo $telp; ?>">
-      </div>
-    </div>
+    
+    
     <div class="form-group">
       <label class="col-sm-3 control-label">No. HP</label>
       <div class="col-sm-9">
-        <input type="text" name="hp" id="hp" class="form-control input-style" placeholder="No. HP . ." value="<?php echo $hp; ?>">
+        <input type="text" name="nomor_hp"  value="<?php echo isset($nomor_hp)?$nomor_hp:""; ?>" id="nomor_hp" class="form-control input-style" placeholder="Nomor HP . . ."  >
       </div>
     </div>
+     
     <div class="form-group">
-      <label class="col-sm-3 control-label">Email</label>
+      <label class="col-sm-3 control-label">Alamat</label>
       <div class="col-sm-9">
-        <input type="text" name="email" id="email" class="form-control input-style" placeholder="Email . . ." value="<?php echo $email; ?>">
+      <textarea name="alamat" id="alamat" class="form-control input-style" placeholder="Alamat . . ."><?php echo isset($alamat)?$alamat:""; ?></textarea>
       </div>
     </div>
+    
     <div class="form-group pull-center">
         <div class="col-sm-offset-3 col-sm-9">
-          <button id="tombolsubmitupdate" style="border-radius: 0;" type="submit" class="btn btn-lg btn-primary"  >Simpan</button>
-          <a href="<?php echo site_url('sa_birojasa'); ?>"><button style="border-radius: 0;" id="reset" type="button" class="btn btn-lg btn-danger">Cancel</button></a>
+          <button id="tombolsubmitsimpan" style="border-radius: 0;" type="submit" class="btn btn-lg btn-primary"  >Simpan</button>
+          <a href="<?php echo site_url("$this->controller"); ?>"><button style="border-radius: 0;" id="reset" type="button" class="btn btn-lg btn-danger">Cancel</button></a>
         </div>
       </div>
 
