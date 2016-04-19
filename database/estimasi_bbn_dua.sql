5cfa93eb5d5569d914c47cc8faf631a3aeb96f04
NizarHafizulllah <nizarhafizullah66@gmail.com>
2016-04-19 08:34:32 +0700

    Data Base Estimasi BBN 2 dan 1

--- database/estimasi_bbn_dua.sql
+++ database/estimasi_bbn_dua.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 12 Apr 2016 pada 05.04
+-- Generation Time: 19 Apr 2016 pada 03.28
 -- Versi Server: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -31,13 +31,20 @@ CREATE TABLE IF NOT EXISTS `estimasi_bbn_dua` (
   `tipe_kendaraan` varchar(100) NOT NULL,
   `tahun_kendaraan` varchar(5) NOT NULL,
   `warna_tnkb` varchar(50) NOT NULL,
-  `polda` int(11) NOT NULL,
-  `samsat` int(11) NOT NULL,
-  `perubahan` int(11) NOT NULL,
+  `id_polda` int(11) NOT NULL,
+  `id_samsat` int(11) NOT NULL,
+  `id_perubahan` int(11) NOT NULL,
   `rp_pendaftaran` varchar(20) NOT NULL,
   `rp_perubahan` varchar(20) NOT NULL,
   `rp_admin_fee` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data untuk tabel `estimasi_bbn_dua`
+--
+
+INSERT INTO `estimasi_bbn_dua` (`id`, `tipe_kendaraan`, `tahun_kendaraan`, `warna_tnkb`, `id_polda`, `id_samsat`, `id_perubahan`, `rp_pendaftaran`, `rp_perubahan`, `rp_admin_fee`) VALUES
+(5, 'JNDK937', '2002', 'BIRU', 16, 10, 4, '21.000', '30.000', '30.000');
 
 --
 -- Indexes for dumped tables
@@ -57,7 +64,7 @@ ALTER TABLE `estimasi_bbn_dua`
 -- AUTO_INCREMENT for table `estimasi_bbn_dua`
 --
 ALTER TABLE `estimasi_bbn_dua`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
