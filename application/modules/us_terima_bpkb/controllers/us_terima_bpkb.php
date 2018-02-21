b62031b6ec6ce7ff831781cd5d703b326c8a7e3d
Nizar Hafizullah <nizarhafizullah66@gmail.com>
2018-02-21 15:10:37 -0800

    index production

--- application/modules/us_terima_bpkb/controllers/us_terima_bpkb.php
+++ application/modules/us_terima_bpkb/controllers/us_terima_bpkb.php
@@ -28,6 +28,9 @@ function index(){
         $data_array=array();
 
         $userdata = $this->session->userdata('user_login');
+
+
+        $data_array['action'] = 'simpan';
         
 
 		$content = $this->load->view($this->controller."_view",$data_array,true);
@@ -39,6 +42,73 @@ function index(){
 		$this->cetak();
 }
 
+
+function get_data(){
+    $post  = $this->input->post();
+    //show_array($post);
+
+   
+
+    $this->db->where('no_mesin', $post['no_mesin']);
+    $this->db->where('bpkb_serah_polda', 1);
+    $this->db->where('status_bpkb', 0);
+    $res = $this->db->get('bj_bbn_satu')->row_array();
+
+    // show_array($res);
+    // echo $this->db->last_query();
+    // exit();
+
+    echo json_encode($res);
+
+    // echo "hasilnya mana ? " . $hasil;
+}
+
+
+function simpan(){
+
+
+    $post = $this->input->post();
+    
+       
+
+
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('no_rangka','No. Rangka','required');    
+        $this->form_validation->set_rules('no_mesin','No. Mesin','required');
+        $this->form_validation->set_rules('no_faktur','No. Faktur','required');
+        $this->form_validation->set_rules('nama_pemilik','Nama Pemilik','required'); 
+        $this->form_validation->set_rules('alamat_pemilik','ALAMAT','required');
+        $this->form_validation->set_rules('bpkb_no','No. BPKB','required');
+        $this->form_validation->set_rules('bpkb_tgl','Tgl. BPKB','required');  
+        $this->form_validation->set_rules('bayar_jumlah_bpkb','Biaya Admin','callback_cek_');    
+        // $this->form_validation->set_rules('pelaksana_nip','NIP','required');         
+         
+        $this->form_validation->set_message('required', ' %s Harus diisi ');
+        
+        $this->form_validation->set_error_delimiters('', '<br>');
+
+     
+        $userdata = $this->session->userdata('bj_login');
+        $post['id_birojasa'] = $userdata['birojasa_id'];
+        //show_array($data);
+
+if($this->form_validation->run() == TRUE ) { 
+
+        
+        $res = $this->db->insert('dealer', $post); 
+        
+        if($res){
+            $arr = array("error"=>false,'message'=>"BERHASIL DISIMPAN");
+        }
+        else {
+             $arr = array("error"=>true,'message'=>"GAGAL  DISIMPAN");
+        }
+}
+else {
+    $arr = array("error"=>true,'message'=>validation_errors());
+}
+        echo json_encode($arr);
+}
 
 
 
---
+++ application/modules/us_terima_bpkb/views/us_terima_bpkb_view_js.php
@@ -0,0 +1,73 @@
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".tanggal").datepicker().on('changeDate', function(ev){                 
+             $('.tanggal').datepicker('hide');
+        });
+
+        $(".rupiah").autoNumeric('init'); 
+
+		 $("#query").click(function(){
+		 	$.ajax({
+            url : '<?php echo site_url("us_terima_bpkb/get_data") ?>',
+           dataType : 'json',
+            type : 'post',
+            data : {  no_mesin : $("#in_mesin").val() },
+            success : function(obj) {
+                // console.log(obj);
+                $("#id").val(obj.id);
+                $("#no_rangka").val(obj.no_rangka);
+                $("#no_mesin").val(obj.no_mesin);
+                $("#no_faktur").val(obj.no_faktur);
+                $("#nama_pemlik").val(obj.nama_pemilik);
+                $("#alamat").val(obj.alamat_pemilik);
+                
+                
+                
+
+            }
+
+       });
+		 });
+
+
+		 $("#simpan").click(function(){
+ console.log('tests');
+
+    $.ajax({
+        url:'<?php echo site_url("$this->controller/simpan"); ?>',
+        data : $('#form_data').serialize(),
+        type : 'post',
+        dataType : 'json',
+        success : function(obj){
+
+            console.log(obj.error);
+
+            if(obj.error == false) { // berhasil 
+
+                // alert('hooooo.. error false');
+                     BootstrapDialog.alert({
+                            type: BootstrapDialog.TYPE_PRIMARY,
+                            title: 'Informasi',
+                            message: obj.message
+                             
+                        });   
+                      $('#form_data').data('bootstrapValidator').resetForm(true);
+                      window.location = "<?php echo site_url('bj_bbn_satu') ?>";
+            }
+            else {
+                 BootstrapDialog.alert({
+                            type: BootstrapDialog.TYPE_DANGER,
+                            title: 'Error',
+                            message: obj.message 
+                             
+                        }); 
+            }
+        }
+    });
+
+    return false;
+});
+
+		 
+	});
+</script>
